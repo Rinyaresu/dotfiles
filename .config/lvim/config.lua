e87a7751a835190ce943c0fcde0930be0ada032e
Your name <kaiquelinhares17@outlook.com>
2022-03-09 02:22:29 -0300

    change colorscheme lvim

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "monokai_soda"
+lvim.colorscheme = "tokyonight"
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
